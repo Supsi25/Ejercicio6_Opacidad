dbae209d5d620e7681703961edfbb79c61fbc868
susana <susana.casas@iesdoctorbalmis.com>
2020-10-26 16:04:27 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio6_Opacidad/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Ejercicio6_Opacidad
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Imagen_Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 1;
+        }
+
+        private void Imagen_Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 0.5;
+        }
+    }
+}
